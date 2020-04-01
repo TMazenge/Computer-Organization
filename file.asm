68c22079febc2a69dc33d02e6a0525289dbcc3e0
Timothy Mazenge <54388897+TMazenge@users.noreply.github.com>
2020-04-01 18:24:14 -0500

    Add files via upload

---
+++ file.asm
@@ -0,0 +1,90 @@
+; Example assembly language program -- adds two numbers
+; Author:  R. Detmer
+; Date:    revised 7/97
+
+.386
+.MODEL FLAT
+
+ExitProcess PROTO NEAR32 stdcall, dwExitCode:DWORD
+
+INCLUDE io.h            ; header file for input/output
+
+cr      EQU     0dh     ; carriage return character
+Lf      EQU     0ah     ; line feed
+
+.STACK  4096            ; reserve 4096-byte stack
+
+.DATA                   ; reserve storage for data
+number1 DWORD   ?
+number2 DWORD   ?
+prompt1 BYTE    "Enter first number:  ", 0
+prompt2 BYTE    "Enter second number:  ", 0
+string  BYTE    40 DUP (?)
+label1  BYTE    cr, Lf, "The sum is "
+sum     BYTE    11 DUP (?)
+        BYTE    cr, Lf, 0
+
+label2  BYTE    cr, Lf, "The difference is "
+diff    BYTE    11 DUP (?)
+        BYTE    cr, Lf, 0
+
+label3  BYTE    cr, Lf, "The product is "
+prod    BYTE    11 DUP (?)
+        BYTE    cr, Lf, 0
+
+label4  BYTE    cr, Lf, "The quotient is "
+quo     BYTE    11 DUP (?)
+        BYTE    cr, Lf, 0
+
+label5  BYTE    cr, Lf, "The remainder is "
+rem     BYTE    11 DUP (?)
+        BYTE    cr, Lf, 0
+
+
+
+.CODE                           ; start of main program code
+_start:
+        output  prompt1         ; prompt for first number
+        input   string, 40      ; read ASCII characters
+        atod    string          ; convert to integer
+        mov     number1, eax    ; store in memory
+
+        output  prompt2         ; repeat for second number
+        input   string, 40
+        atod    string
+        mov     number2, eax
+        
+        mov     eax, number1    ; first number to EAX
+        add     eax, number2    ; add second number
+        dtoa    sum, eax        ; convert to ASCII characters
+        output  label1          ; output label and sum
+
+        mov     eax, number1    ; first number to EAX
+        sub     eax, number2    ; add second number
+        dtoa    diff, eax        ; convert to ASCII characters
+        output  label2         ; output label and sum
+
+        mov     eax, number1    ; first number to EAX
+        mul     number2    ; add second number
+        dtoa    prod, eax        ; convert to ASCII characters
+        output  label3         ; output label and sum
+
+        mov     eax, number1    ; first number to EAX
+        move    edx, 0
+        div     eax, number2    ; add second number
+        dtoa    quo, eax        ; convert to ASCII characters
+        output  label4        ; output label and sum
+
+        dtoa    rem, edx     ; convert to ASCII characters
+        output  label4        ; output label and sum
+
+
+
+
+
+        INVOKE  ExitProcess, 0  ; exit with return code 0
+
+PUBLIC _start                   ; make entry point public
+
+END                             ; end of source code
+
